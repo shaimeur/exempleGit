760f5761e99f94fefdad1a0e6b7e6b1a0a8df5fa
shaimeur <haimeursaad@gmail.com>
2023-10-11 15:26:19 +0100

    mise à jour du project

---
+++ app.js
@@ -0,0 +1,24 @@
+// let x = 0 ; // Variable of type Number
+
+// let phrase = "We Are GomyCode" // variable of Type String
+
+// let bool = true // variable of type Boolien
+
+
+
+
+// console.log(x);
+// console.log(phrase);
+// console.log(bool);
+
+
+
+
+// let var1 = 56;
+// let var2 = 5 ;
+
+// console.log(var1 != var2)
+
+// confirm("wesh fater")
+
+prompt("what is your age?")
